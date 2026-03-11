7ad1c476715a5fe714f15d60b28eba39c5f20de6
Rafael Gustavo Gallegos Tapia <167254500+RafaGGT@users.noreply.github.com>
2026-03-11 19:33:58 -0300

    Add allowed origin for CORS configuration

--- src/main/java/com/dev/rggt/agendaspring/infra/config/CorsConfig.java
+++ src/main/java/com/dev/rggt/agendaspring/infra/config/CorsConfig.java
@@ -15,6 +15,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("https://contactos-springboot-react-backend.onrender.com")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
